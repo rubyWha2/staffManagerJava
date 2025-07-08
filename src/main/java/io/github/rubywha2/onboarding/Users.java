030fbb0a2bfa811e0e9b473f55c5a5e88897b6e5
xxxru <r.m.wharton2@newcastle.ac.uk>
2025-07-08 22:57:04 +0100

    Users Class has been created

---
+++ src/main/java/io/github/rubywha2/onboarding/Users.java
@@ -0,0 +1,25 @@
+package io.github.rubywha2.onboarding;
+
+public class Users
+{
+    private String username;
+    private String password;
+    private String role;
+
+    public Users() {}
+
+    public Users(int id, String username, String passwordHash) {
+        this.role = role;
+        this.username = username;
+        this.password = passwordHash;
+    }
+    public String getRole(){return role;}
+    public void setRole(String role){this.role = role;}
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPasswordHash() { return password ; }
+    public void setPasswordHash(String passwordHash) { password = password; }
+}
+
