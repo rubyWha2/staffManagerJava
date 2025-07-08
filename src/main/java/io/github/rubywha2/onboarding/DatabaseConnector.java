743aeeadcf43bf5c4ecb1b342c87af84090a6b09
xxxru <r.m.wharton2@newcastle.ac.uk>
2025-07-08 22:45:12 +0100

    Database Connection set up, pom has mySQL dependency

---
+++ src/main/java/io/github/rubywha2/onboarding/DatabaseConnector.java
@@ -0,0 +1,15 @@
+package io.github.rubywha2.onboarding;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnector {
+    private static final String URL = "jdbc:mysql://localhost:3306/StaffOnboarding";
+    private static final String USER = "ruby";
+    private static final String PASSWORD = "happy";
+
+    public static Connection connect() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
