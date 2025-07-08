d2747ce05f18fc7e40114280bb95f77cfe2d57c3
xxxru <r.m.wharton2@newcastle.ac.uk>
2025-07-08 22:26:27 +0100

    Main Created

---
+++ src/main/java/io/github/rubywha2/onboarding/Main.java
@@ -0,0 +1,7 @@
+package io.github.rubywha2.onboarding;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Secure Staff Onboarding Program started.");
+    }
+}
