02ec7889d6d5ac48a40fb380034ecfb9916057d9
xxxru <r.m.wharton2@newcastle.ac.uk>
2025-07-13 23:26:38 +0100

    Staff Training Class has been created and its basic methods completed.

---
+++ src/main/java/io/github/rubywha2/onboarding/Training.java
@@ -0,0 +1,41 @@
+package io.github.rubywha2.onboarding;
+
+public class Training {
+    private String trainingCourseName;
+    private String trainingDescription;
+    private String trainingProvider;
+    private String trainingID;
+
+    Training(String trainingCourseName, String trainingDescription, String trainingProvider, String trainingID) {
+        this.trainingCourseName = trainingCourseName;
+        this.trainingDescription = trainingDescription;
+        this.trainingProvider = trainingProvider;
+        this.trainingID = trainingID;
+    }
+
+    public String getTrainingCourseName(){
+        return trainingCourseName;
+    }
+    public void setTrainingCourseName(String trainingCourseName){
+        this.trainingCourseName = trainingCourseName;
+    }
+    public String getTrainingDescription(){
+        return trainingDescription;
+    }
+    public void setTrainingDescription(String trainingDescription){
+        this.trainingDescription = trainingDescription;
+    }
+    public String getTrainingProvider(){
+        return trainingProvider;
+    }
+    public void setTrainingProvider(String trainingProvider){
+        this.trainingProvider = trainingProvider;
+    }
+    public String getTrainingID(){
+        return trainingID;
+    }
+    public void setTrainingID(String trainingID){
+        this.trainingID = trainingID;
+    }
+
+}
