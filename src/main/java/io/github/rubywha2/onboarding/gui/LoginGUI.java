e08050a5613fd3c15f2659dc1dd724dc76b566b2
xxxru <r.m.wharton2@newcastle.ac.uk>
2025-07-14 23:24:25 +0100

    LogInGUI has been updated to load log in page, which asks for username and password.

---
+++ src/main/java/io/github/rubywha2/onboarding/gui/LoginGUI.java
@@ -0,0 +1,36 @@
+package io.github.rubywha2.onboarding.gui;
+import javax.swing.*;
+
+public class LoginGUI {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Login");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1100, 800);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(null); // Manual layout
+        frame.add(panel);
+        frame.setVisible(true);
+
+        JLabel userLabel = new JLabel("Username:");
+        userLabel.setBounds(10, 20, 80, 25);
+        panel.add(userLabel);
+
+        JTextField userText = new JTextField(20);
+        userText.setBounds(100, 20, 165, 25);
+        panel.add(userText);
+
+        JLabel passwordLabel = new JLabel("Password:");
+        passwordLabel.setBounds(10, 50, 80, 25);
+        panel.add(passwordLabel);
+
+        JPasswordField passwordText = new JPasswordField(20);
+        passwordText.setBounds(100, 50, 165, 25);
+        panel.add(passwordText);
+
+        JButton loginButton = new JButton("Login");
+        loginButton.setBounds(10, 80, 80, 25);
+        panel.add(loginButton);
+
+    }
+}
