7439659f4d42b1067d1541a86ab052b631702230
xxxru <r.m.wharton2@newcastle.ac.uk>
2025-07-20 20:28:28 +0100

    feat(DAO): staff DAO now connects to database and returns list of Staff details

--- src/main/java/io/github/rubywha2/onboarding/dao/StaffDAO.java
+++ src/main/java/io/github/rubywha2/onboarding/dao/StaffDAO.java
@@ -1,4 +1,42 @@
 package io.github.rubywha2.onboarding.dao;
+import io.github.rubywha2.onboarding.Database.DatabaseConnector;
+import io.github.rubywha2.onboarding.model.Users;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StaffDAO {
+
+    public List<Users> getStaffData() {
+        List<Users> staffList = new ArrayList<>();
+
+        String query = "SELECT Firstname, Lastname, Email, Postcode,StaffID, DBS_Number, RoleID  FROM Staff";
+
+        try (Connection conn = DatabaseConnector.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query);
+             ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()) {
+                Users staff = new Users(
+                        rs.getString("Firstname"),
+                        rs.getString("Lastname"),
+                        rs.getString("Email"),
+                        rs.getString("Postcode"),
+                        rs.getString("StaffID"),
+                        rs.getInt("DBS_Number"),
+                        rs.getString("RoleID")
+                );
+                staffList.add(staff);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace(); // Replace with logging later
+        }
+
+        return staffList;
+    }
 }
