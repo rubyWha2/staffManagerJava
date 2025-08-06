b1be591e42e844b89bf9650ee44545f19afa598c
xxxru <r.m.wharton2@newcastle.ac.uk>
2025-08-06 20:08:03 +0100

    Main class takes input from the user to decide if the login records from the cvs file are displayed.

--- src/main/java/io/github/rubywha2/onboarding/Main.java
+++ src/main/java/io/github/rubywha2/onboarding/Main.java
@@ -1,13 +1,25 @@
 package io.github.rubywha2.onboarding;
 import io.github.rubywha2.onboarding.gui.LoginGUI;
 import io.github.rubywha2.onboarding.service.LoginService;
+import io.github.rubywha2.onboarding.util.CSVLogger;
+import java.util.Scanner;
 
 
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Secure Staff Onboarding Program started.");
-
+        CSVLogger logger = new CSVLogger();
         LoginService loginService = new LoginService();
         new LoginGUI(loginService);
+
+        System.out.println("Please enter Y to view user log in records...");
+        Scanner scanner = new Scanner(System.in);
+        String viewLogs = scanner.nextLine();
+        scanner.close();
+        if (viewLogs.equals("Y")) {
+            logger.printLogs();
+        }
+
     }
 }
