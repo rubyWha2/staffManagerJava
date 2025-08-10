7b871a641173a36d412dafb59b19cb98d2fcaf7e
xxxru <r.m.wharton2@newcastle.ac.uk>
2025-08-10 20:12:31 +0100

    Databaseconnector package now has the appropriate comments.

--- src/main/java/io/github/rubywha2/onboarding/Database/DatabaseConnector.java
+++ src/main/java/io/github/rubywha2/onboarding/Database/DatabaseConnector.java
@@ -4,12 +4,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * Utility class responsible for establishing and providing
+ * connections to the StaffOnboarding MySQL database.
+ */
 public class DatabaseConnector {
     private static final String URL = "jdbc:mysql://localhost:3306/StaffOnboarding";
     private static final String USER = "ruby";
     private static final String PASSWORD = "happy";
 
-    public static Connection getConnection()  throws SQLException {
+    /**
+     * Creates and returns a new database connection.
+     *
+     * @return a Connection object connected to the database.
+     * @throws SQLException if a database access error occurs.
+     */
+    public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
