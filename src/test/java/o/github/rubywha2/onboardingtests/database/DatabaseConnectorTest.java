2d844862296c7a1f487529ed64dee9981c1c0061
xxxru <r.m.wharton2@newcastle.ac.uk>
2025-08-12 20:43:28 +0100

    DatabaseConnector has now been tested, and passed via DatabaseConnectorTest.java.

--- src/test/java/o/github/rubywha2/onboardingtests/database/DatabaseConnectorTest.java
+++ src/test/java/o/github/rubywha2/onboardingtests/database/DatabaseConnectorTest.java
@@ -1,4 +1,16 @@
 package o.github.rubywha2.onboardingtests.database;
+import org.junit.jupiter.api.Test;
+import java.sql.Connection;
+import java.sql.SQLException;
+import io.github.rubywha2.onboarding.Database.DatabaseConnector;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class DatabaseConnectorTest {
+class DatabaseConnectorTest {
+
+    @Test
+    void testGetConnection() throws SQLException {
+        try (Connection conn = DatabaseConnector.getConnection()) {
+            assertTrue(conn.isValid(2), "Connection should be valid");
+        }
+    }
 }
